1dc7d70110e8a2a2c29f0f7ed3c1b289a3430f3d
Oreki <35621834+Orekiz@users.noreply.github.com>
2023-03-18 19:58:24 +0800

    chore: 更新页面样式

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -10,11 +10,11 @@
   <main w-full h-full flex justify-center items-center>
     <div z--1 fixed top-0 w-screen h-screen bg-bottom bg-no-repeat class="backgroundimg lt-md:bg-cover"></div>
     <section container max-w="2xl" class="lt-md:p-10">
-      <h1 lh-10 font-black text-5xl bg-transparent hover="bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent" transition cursor-default>
+      <h1 lh-10 font-black text-9 bg-transparent hover="bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent" transition cursor-default>
         Oreki
       </h1>
-      <p indent-2 lh-10>no bio here.</p>
-      <section flex="~ gap-5" mt-5 text-8xl>
+      <p indent-1 lh-10>no bio here.</p>
+      <section flex="~ gap-5" mt-5 text-14>
         <a :href="item.url" v-for="(item, index) in links" :key=index :class="item.icon" hover="bg-gradient-to-r from-cyan-500 to-blue-500" cursor-pointer target="_blank"></a>
       </section>
     </section>

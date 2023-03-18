1c87b92ac922a5f3f93872c8b32c463c20d45aba
Oreki <35621834+Orekiz@users.noreply.github.com>
2023-03-19 03:12:53 +0800

    fix: 修复暗黑模式下copyright颜色仍为黑色

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -5,9 +5,9 @@ function toggleDarkHandler() {
 </script>
 
 <template>
-    <footer border-t fixed flex justify-center items-center bottom-0 left-0 w-screen h-16 bg-transparent>
+    <footer text="gray-800 dark:gray-100" border-t fixed flex justify-center items-center bottom-0 left-0 w-screen h-16 bg-transparent>
         <div my-max-width ma gap-4 flex justify-center items-center>
-            <button block p-3 text="gray-800 dark:gray-100" @click="toggleDarkHandler" i-mdi-white-balance-sunny
+            <button block p-3 @click="toggleDarkHandler" i-mdi-white-balance-sunny
                     dark:i-mdi-weather-night-partly-cloudy />
             <p inline-block>2023-PRESENT &copy; Oreki</p>
         </div>
